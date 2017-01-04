a69c45bdb4f7f8b5a2c8e52ea8213546b08669fc
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-04 12:44:11 -0700

    monir text fixes

--- spec/models/outing_spec.rb
+++ spec/models/outing_spec.rb
@@ -1,45 +1,45 @@
 require 'rails_helper'
 
 describe Outing do
-  describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a title" do
-        outing = Outing.create(description: "thing", image_url: "thingy", base_cost: 4)
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a title' do
+        outing = Outing.create(description: 'thing', image_url: 'thingy', base_cost: 4)
 
         expect(outing).to be_invalid
       end
 
-      it "is invalid without a description" do
-        outing = Outing.create(title: "thing", image_url: "thingy4", base_cost: 4)
+      it 'is invalid without a description' do
+        outing = Outing.create(title: 'thing', image_url: 'thingy4', base_cost: 4)
 
         expect(outing).to be_invalid
       end
 
-      it "is invalid without an image" do
-        outing = Outing.create(title: "thing", description: "thingy3", base_cost: 4)
+      it 'is invalid without an image' do
+        outing = Outing.create(title: 'thing', description: 'thingy3', base_cost: 4)
 
         expect(outing).to be_invalid
       end
 
-      it "is invalid without base cost" do
-        outing = Outing.create(title: "thing", image_url: "thingy1", image_url: "bob")
+      it 'is invalid without base cost' do
+        outing = Outing.create(title: 'thing', description: 'thingy1', image_url: 'bob')
 
         expect(outing).to be_invalid
       end
     end
 
-    context "valid attributes" do
-      it "is valid" do
-        outing = Outing.create(title: "thing", description: "thingy3", image_url: "thingy2", image_url: "bob", base_cost: 4)
+    context 'valid attributes' do
+      it 'is valid' do
+        outing = Outing.create(title: 'thing', description: 'thingy3', image_url: 'bob', base_cost: 4)
 
         expect(outing).to be_valid
       end
     end
   end
 
-  describe "relationships" do
-    it "belongs to a politician" do
-      outing = Outing.create(title: "thing", image_url: "thingy", image_url: "bob")
+  describe 'relationships' do
+    it 'belongs to a politician' do
+      outing = Outing.create(title: 'thing', description: 'thingy3', image_url: 'bob', base_cost: 4)
 
       expect(outing).to respond_to(:politician)
     end
--- spec/models/politician_spec.rb
+++ spec/models/politician_spec.rb
@@ -1,52 +1,52 @@
 require 'rails_helper'
 
 describe Politician do
-  describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a name" do
-        politician = Politician.create(party: "demo", multiplier: 2, image: "thingy")
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a name' do
+        politician = Politician.create(party: 'demo', multiplier: 2, image: 'thingy')
 
         expect(politician).to be_invalid
       end
 
-      it "has a unique name" do
-        politician = Politician.create(name: "Daniel", party: "demo", multiplier: 2, image: "thingy")
-        politician_2 = Politician.create(name: "Daniel", party: "demo", multiplier: 2, image: "thingy")
+      it 'has a unique name' do
+        politician = Politician.create(name: 'Daniel', party: 'demo', multiplier: 2, image: 'thingy')
+        politician_2 = Politician.create(name: 'Daniel', party: 'demo', multiplier: 2, image: 'thingy')
 
         expect(politician_2).to be_invalid
       end
 
-      it "is invalid without a party" do
-        politician = Politician.create(name: "Daniel", multiplier: 2, image: "thingy")
+      it 'is invalid without a party' do
+        politician = Politician.create(name: 'Daniel', multiplier: 2, image: 'thingy')
 
         expect(politician).to be_invalid
       end
 
-      it "is invalid without an image" do
-        politician = Politician.create(name: "Daniel", party: "demo", multiplier: 2)
+      it 'is invalid without an image' do
+        politician = Politician.create(name: 'Daniel', party: 'demo', multiplier: 2)
 
         expect(politician).to be_invalid
       end
 
-      it "is invalid without a multiplier" do
-        politician = Politician.create(name: "Daniel", party: "demo", image: "thingy")
+      it 'is invalid without a multiplier' do
+        politician = Politician.create(name: 'Daniel', party: 'demo', image: 'thingy')
 
         expect(politician).to be_invalid
       end
     end
 
-    context "valid attributes" do
-      it "is has all valid attributes" do
-        politician = Politician.create(name: "Daniel", party: "demo", multiplier: 2, image: "thingy")
+    context 'valid attributes' do
+      it 'is has all valid attributes' do
+        politician = Politician.create(name: 'Daniel', party: 'demo', multiplier: 2, image: 'thingy')
 
         expect(politician).to be_valid
       end
     end
   end
 
-  describe "relationships" do
-    it "has many outings" do
-      politician = Politician.create!(name: "Daniel", party: "demo", multiplier: 2, image: "thingy")
+  describe 'relationships' do
+    it 'has many outings' do
+      politician = Politician.create!(name: 'Daniel', party: 'demo', multiplier: 2, image: 'thingy')
 
       expect(politician).to respond_to(:outings)
     end

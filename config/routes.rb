dc6899e62e55da1648389c320daac86191467af0
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-05 00:27:44 -0700

    Add Politicians, Outings to Navbar to improve usability, temporarily configure Root to redirect to Outings Index View

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root :to => 'outings#index'  
   resources 'politicians', only: [:index]
   resources :outings, only: [:index]
   get '/cart' => 'carts#index'

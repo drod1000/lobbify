a205fe3a64ab0bf1a20aab038cb5c00eb73cf33d
Daniel Rodriguez <drod2793@gmail.com>
2017-01-05 10:02:33 -0700

    Removes politician index

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,8 +1,5 @@
 class PoliticiansController < ApplicationController
-  def index
-    @politicians = Politician.all
-  end
-
+  
   def show
     @politician = Politician.find_by(name: params[:name])
   end

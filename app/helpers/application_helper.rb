6bd77abb5789601cca829e5889ae54146e7e0d5b
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-05 20:00:43 -0700

    Remove cart total and detailed content preparation from controller to Cart model. Create additional Cart model tests, update view accordingly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def path_cleaner(path)
-    path.downcase.gsub("_"," ").titleize
+    path.downcase.gsub('_',' ').titleize
+  end
+
+  def path_creator(name)
+    name.downcase.titleize.gsub(' ','_')
   end
 end
--- spec/helpers/path_cleaner_spec.rb
+++ spec/helpers/path_cleaner_spec.rb
@@ -6,23 +6,24 @@ RSpec.describe "Method path_cleaner" do
       include ApplicationHelper
     end
     @tester = PathCleanerTest.new
+    @expected_output = "Some Name"
   end
 
-  describe "it replaces speces" do
+  describe "it adds spaces" do
     scenario "input is all Capitalized" do
       test_path = "Some_Name"
 
-      expect(@tester.path_cleaner(test_path)).to eq("Some Name")
+      expect(@tester.path_cleaner(test_path)).to eq(@expected_output)
     end
     scenario "input is not capitalized" do
       test_path = "some_name"
 
-      expect(@tester.path_cleaner(test_path)).to eq("Some Name")
+      expect(@tester.path_cleaner(test_path)).to eq(@expected_output)
     end
     scenario "input is upper case and lower case mixed" do
       test_path = "sOMe_naME"
 
-      expect(@tester.path_cleaner(test_path)).to eq("Some Name")
+      expect(@tester.path_cleaner(test_path)).to eq(@expected_output)
     end
   end
 end

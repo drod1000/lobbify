19ad443e5d4ab663fa3f069584d1101c04b76bdb
Daniel Rodriguez <drod2793@gmail.com>
2017-01-06 20:34:44 -0700

    Adds subtotal test for order_item

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -28,4 +28,14 @@ describe OrderItem do
       expect(order_item).to respond_to(:outing)
     end
   end
+
+  describe 'methods' do
+    it 'can calculate subtotal' do
+      order = create(:order)
+      outing = create(:outing)
+      order_item = OrderItem.create(order_id: order.id, outing_id: outing.id, quantity: 5)
+
+      expect(order_item.subtotal).to eq((outing.adjusted_cost * 5))
+    end
+  end
 end

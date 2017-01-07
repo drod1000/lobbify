16cac833878b6f1603b0fa8c248156c5b53b2873
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-07 07:47:01 -0700

    Expand test coverage for all order status ordered, paid, cancelled and completed

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
 
-  enum status: %w(ordered paid cancelled shipped)
+  enum status: %w(ordered paid cancelled shipped completed)
 
   def total
     order_items.reduce(0) do |sum, order_item|
--- spec/features/orders/user_can_view_a_past_order_spec.rb
+++ spec/features/orders/user_can_view_a_past_order_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'user can view a past order' do
-  scenario 'from own orders' do
+  scenario 'from own orders with ordered status' do
     test_user1, test_user2 = create_list(:user, 2)
     order1, order2 = create_list(:order, 2, user: test_user1)
     order3, order4 = create_list(:order, 2, user: test_user2)
+    order1.status = "ordered"
+    order1.save
     outing = create(:outing)
     order_item = OrderItem.create(order_id: order1.id, outing_id: outing.id, quantity: 2)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user1)
@@ -22,5 +24,81 @@ RSpec.describe 'user can view a past order' do
     expect(page).to have_content(order1.status)
     expect(page).to have_content(order1.total)
     expect(page).to have_content(order1.created_at)
+    expect(page).not_to have_content(" on #{order1.updated_at}")
+  end
+  scenario 'from own orders with paid status' do
+    test_user1, test_user2 = create_list(:user, 2)
+    order1, order2 = create_list(:order, 2, user: test_user1)
+    order3, order4 = create_list(:order, 2, user: test_user2)
+    order1.status = "paid"
+    order1.save
+    outing = create(:outing)
+    order_item = OrderItem.create(order_id: order1.id, outing_id: outing.id, quantity: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user1)
+
+    visit orders_path
+
+    within("table") do
+      expect(page).to have_content("#{order1.id}")
+      click_on "#{order1.id}"
+    end
+
+    expect(current_path).to eq(order_path(order1))
+    expect(page).to have_content(outing.title)
+    expect(page).to have_content(order_item.subtotal)
+    expect(page).to have_content(order1.status)
+    expect(page).to have_content(order1.total)
+    expect(page).to have_content(order1.created_at)
+    expect(page).not_to have_content(" on #{order1.updated_at}")
+  end
+  scenario 'from own orders with completed status' do
+    test_user1, test_user2 = create_list(:user, 2)
+    order1, order2 = create_list(:order, 2, user: test_user1)
+    order3, order4 = create_list(:order, 2, user: test_user2)
+    order1.status = "completed"
+    order1.save
+    outing = create(:outing)
+    order_item = OrderItem.create(order_id: order1.id, outing_id: outing.id, quantity: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user1)
+
+    visit orders_path
+
+    within("table") do
+      expect(page).to have_content("#{order1.id}")
+      click_on "#{order1.id}"
+    end
+
+    expect(current_path).to eq(order_path(order1))
+    expect(page).to have_content(outing.title)
+    expect(page).to have_content(order_item.subtotal)
+    expect(page).to have_content(order1.status)
+    expect(page).to have_content(order1.total)
+    expect(page).to have_content(order1.created_at)
+    expect(page).to have_content(" on #{order1.updated_at}")
+  end
+  scenario 'from own orders with cancelled status' do
+    test_user1, test_user2 = create_list(:user, 2)
+    order1, order2 = create_list(:order, 2, user: test_user1)
+    order3, order4 = create_list(:order, 2, user: test_user2)
+    order1.status = "cancelled"
+    order1.save
+    outing = create(:outing)
+    order_item = OrderItem.create(order_id: order1.id, outing_id: outing.id, quantity: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user1)
+
+    visit orders_path
+
+    within("table") do
+      expect(page).to have_content("#{order1.id}")
+      click_on "#{order1.id}"
+    end
+
+    expect(current_path).to eq(order_path(order1))
+    expect(page).to have_content(outing.title)
+    expect(page).to have_content(order_item.subtotal)
+    expect(page).to have_content(order1.status)
+    expect(page).to have_content(order1.total)
+    expect(page).to have_content(order1.created_at)
+    expect(page).to have_content(" on #{order1.updated_at}")
   end
 end

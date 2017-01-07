55c8991a52d8c6d8f1fb5c458e41c36548c7cc7a
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-07 16:55:51 -0700

    Add test and update sessions_controller for use case when email that is not in user database is entered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:success] = "Successfully logged in!"
       redirect_to dashboard_path
     else
-      flash[:success] = @user.errors.full_messages
+      flash[:danger] = "Failed login"
       redirect_to login_path
     end
   end
--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "User log in" do
   before(:each) do
     @user = User.create(name: "Daniel", email: "d@d.com", password: "password")
   end
-  it "can log in from home page" do
+  it "can log in with proper credentials from home page" do
     visit root_path
 
     expect(page).to have_link("Login")
@@ -25,5 +25,24 @@ RSpec.feature "User log in" do
     expect(page).to have_content("Email: d@d.com")
     expect(page).to_not have_link("Login")
     expect(page).to have_link("Logout")
+  end
+  it "cannot log in with incorrect email from home page" do
+    visit root_path
+
+    expect(page).to have_link("Login")
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+
+    within("form") do
+      fill_in "email", with: "incorrect_email"
+      fill_in "password", with: @user.password
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Failed login')
+    expect(page).to have_link('Login')
+    expect(page).not_to have_link('Logout')
   end
 end

2799753a2277582ea1c6995e47ec59aa0fb61fc9
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-07 16:04:55 -0700

    add admin functionality create admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
-  
+
+  enum role: %w(default admin)
+
   has_secure_password
 
   has_many :orders
--- spec/features/admin/admin_can_create_an_admin_spec.rb
+++ spec/features/admin/admin_can_create_an_admin_spec.rb
@@ -5,20 +5,22 @@ describe "Admin logs in" do
     admin = User.create(name: "Drew", email: "email@email.com", password: "password", role: 1)
     user  = User.create(name: "Laszlo", email: "email2@email.com", password: "1password", role: 0)
 
+    page.set_rack_session(user_id: admin.id)
+
     visit admin_users_path
 
-    within "table" do
+    within "tr:nth-of-type(3)" do
       expect(page).to have_content("Laszlo")
-      expect(page).to have_content("Default")
+      expect(page).to have_content("default")
     end
 
-    within "table" do
+    within "tr:nth-of-type(3)" do
       click_on "Make Admin"
     end
 
-    within "table" do
+    within "tr:nth-of-type(3)" do
       expect(page).to have_content("Laszlo")
-      expect(page).to have_content("Admin")
+      expect(page).to have_content("admin")
       expect(page).to_not have_content("Make Admin")
     end
     expect(current_path).to eq(admin_users_path)

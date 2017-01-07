f3b7059a939ddea4eb6639ba8665a54e3607bf2a
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-07 16:09:00 -0700

    test default users ability to access admin funcitonality

--- spec/features/users/authenticated_user_security_spec.rb
+++ spec/features/users/authenticated_user_security_spec.rb
@@ -1,22 +1,15 @@
 require 'rails_helper'
 
 describe "Authenticated User logs in" do
-  scenario "and cannot view the admin" do
-    user1, user2 = create_list(:user, 2)
-    order1, order2 = create_list(:order, 2, user: user1)
-    order3, order4 = create_list(:order, 2, user: user2)
-    outing1, outing2, outing3, outing4 = create_list(:outing, 4)
-    order_outing1 = OrderOuting.create(order_id: order1.id, outing_id: outing1.id, quantity: 1)
-    order_outing2 = OrderOuting.create(order_id: order1.id, outing_id: outing2.id, quantity: 2)
-    order_outing3 = OrderOuting.create(order_id: order3.id, outing_id: outing3.id, quantity: 3)
-    order_outing4 = OrderOuting.create(order_id: order3.id, outing_id: outing4.id, quantity: 4)
+  scenario "and cannot make themselves an admin" do
+    admin = User.create(name: "Drew", email: "email@email.com", password: "password", role: 1)
+    user  = User.create(name: "Laszlo", email: "email2@email.com", password: "1password", role: 0)
 
-    page.set_rack_session(user_id: user1.id)
-    # save_and_open_page
+    page.set_rack_session(user_id: user.id)
 
-    visit '/orders'
+    visit '/admin/users'
 
-
-
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    expect(page).to_not have_content("Make Admin")
   end
 end

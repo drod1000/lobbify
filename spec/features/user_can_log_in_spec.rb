ae95460901515131073872d5e5971840c06a42f4
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-07 17:02:49 -0700

    Add test case when user try to login with email from another user

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "User log in" do
 
   before(:each) do
     @user = User.create(name: "Daniel", email: "d@d.com", password: "password")
+    @user2 = User.create(name: "user2", email: "user2@user2.com", password: "user2")
   end
   it "can log in with proper credentials from home page" do
     visit root_path
@@ -26,7 +27,7 @@ RSpec.feature "User log in" do
     expect(page).to_not have_link("Login")
     expect(page).to have_link("Logout")
   end
-  it "cannot log in with incorrect email from home page" do
+  it "cannot log in from home page with email that is not in user database" do
     visit root_path
 
     expect(page).to have_link("Login")
@@ -36,6 +37,25 @@ RSpec.feature "User log in" do
 
     within("form") do
       fill_in "email", with: "incorrect_email"
+      fill_in "password", with: @user.password
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Failed login')
+    expect(page).to have_link('Login')
+    expect(page).not_to have_link('Logout')
+  end
+  it "cannot log in from home page with email of another user" do
+    visit root_path
+
+    expect(page).to have_link("Login")
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+
+    within("form") do
+      fill_in "email", with: @user2.email
       fill_in "password", with: @user.password
       click_on "Login"
     end

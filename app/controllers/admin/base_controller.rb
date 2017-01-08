45c14aebaab2b2c5db66bb332ae334dbef39e70c
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-07 17:08:28 -0700

    test visitor admin functionality blocked

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,7 +3,7 @@ class Admin::BaseController < ApplicationController
   before_action :require_admin
 
   def require_admin
-    render file: "/public/404" unless current_user.admin?
+    render file: "/public/404" unless current_user && current_user.admin?
   end
 
 end

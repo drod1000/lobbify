0ac795892af8e73372187deb294bc9c0ae33b7a2
njgheorghita <njgheorghita@email.wm.edu>
2017-01-09 14:56:32 -0700

    admin edit outing function

--- app/controllers/admin/outings_controller.rb
+++ app/controllers/admin/outings_controller.rb
@@ -9,13 +9,21 @@ class Admin::OutingsController < Admin::BaseController
   end
 
   def update
-
+    @outing = Outing.find(params[:id])
+    @outing.update(outing_params)
+    if @outing.save
+      flash[:success] = "Successfully updated #{@outing.title} with #{@outing.politician.name}"
+      redirect_to @outing
+    else
+      flash[:danger] = "Must populate all fields before updating"
+      render :edit
+    end
   end
 
   private
 
   def outing_params
-    params.require(:outing).permit(:title, :description, :base_cost)
+    params.require(:outing).permit(:title, :description, :base_cost, :image_url, :status)
   end
 
 end
--- spec/features/admin/admin_edits_an_outing_spec.rb
+++ spec/features/admin/admin_edits_an_outing_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Admin" do
-  scenario "edits and existing outing" do
+  scenario "successfully edits an existing outing" do
     politician = Politician.create(name: 'DTrump', party: 'R', multiplier: 2, image: '/images/golf.jpg')
     outing = politician.outings.create(title: 'test', description: 'test-description', image_url: '/images/golf.jpg', base_cost: 4, status: 0)
     admin = User.create(name: "Drew", email: "email@email.com", password: "password", role: 1)
@@ -27,5 +27,27 @@ describe "Admin" do
     expect(page).to have_content("Ride Bike")
     expect(page).to_not have_content("test-description")
     expect(page).to have_content(politician.name)
+  end
+
+  scenario "unsuccessfully edits an existing outing" do
+    politician = Politician.create(name: 'DTrump', party: 'R', multiplier: 2, image: '/images/golf.jpg')
+    outing = politician.outings.create(title: 'test', description: 'test-description', image_url: '/images/golf.jpg', base_cost: 4, status: 0)
+    admin = User.create(name: "Drew", email: "email@email.com", password: "password", role: 1)
+
+    page.set_rack_session(user_id: admin.id)
+
+    visit '/admin/outings'
+
+    click_on "Edit"
+
+    expect(current_path).to eq(edit_admin_outing_path(outing))
+
+    within("form") do
+      fill_in 'outing[title]', with: ""
+      click_on "Update Outing"
+    end
+    expect(current_path).to eq(admin_outing_path(outing))
+    expect(page).to have_content("Must populate all fields before updating")
+    expect(page).not_to have_content("Biking")
   end
 end

418bf18c8d4abc69cc1c1412e46831f8e5845654
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-11 14:53:54 -0700

    admin outing controller refactored

--- app/controllers/admin/outings_controller.rb
+++ app/controllers/admin/outings_controller.rb
@@ -1,17 +1,16 @@
 class Admin::OutingsController < Admin::BaseController
 
+  before_action :set_outing, only: [:edit, :update]
+
   def index
     @search = Outing.search(params[:q])
     @outings = @search.result
-    # @outings = Outing.all
   end
 
   def edit
-    @outing = Outing.find(params[:id])
   end
 
   def update
-    @outing = Outing.find(params[:id])
     @outing.update(outing_params)
     if @outing.save
       flash[:success] = "Successfully updated #{@outing.title} with #{@outing.politician.name}"
@@ -26,6 +25,10 @@ class Admin::OutingsController < Admin::BaseController
 
   def outing_params
     params.require(:outing).permit(:title, :description, :base_cost, :image_url, :status)
+  end
+
+  def set_outing
+    @outing = Outing.find(params[:id])
   end
 
 end
